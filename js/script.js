c293c9ea133abe71cabeb9bf33562042001c1837
Shabz614 <149861325+Shabz614@users.noreply.github.com>
2023-12-12 08:52:11 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".teachers-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+  });
